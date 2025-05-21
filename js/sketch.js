e5f8c8f5cae662a32dfd9dda698386053048ec2b
Lenny <cucukazu@gmail.com>
2025-05-21 10:29:38 -0400

    optimize the play/pause button

--- js/sketch.js
+++ js/sketch.js
@@ -2,34 +2,38 @@
 //Original idea + tutorial: https://youtu.be/8O5aCwdopLo?si=s1detCu0abm2zs2s
 
 var song
-let songs = ['mp3/renai.mp3', 'mp3/almondChoco.mp3', 'mp3/magnetic.mp3', 'mp3/seeLove.mp3', 'mp3/hyperR.mp3', 'mp3/tellUrWorld.mp3', 'mp3/kawaiiRemix.mp3', 'mp3/suMuZheRemix.mp3', 'mp3/2mins.mp3', 'mp3/bukanPho.mp3']; 
+let songs = ['mp3/renai.mp3', 'mp3/almondChoco.mp3', 'mp3/magnetic.mp3', 'mp3/seeLove.mp3', 'mp3/hyperR.mp3', 'mp3/tellUrWorld.mp3', 'mp3/kawaiiRemix.mp3', 'mp3/suMuZheRemix.mp3', 'mp3/2mins.mp3', 'mp3/bukanPho.mp3'];
 let currentSongIndex = 0;
 let changeSongButton;
 
 function preload() {
-    song =loadSound(songs[currentSongIndex]);
+    song = loadSound(songs[currentSongIndex]);
     console.log('Now playing:', songs[currentSongIndex]);
 
 }
 
 var canvas;
 let playButton, pauseButton;
-let smoothing=0.8;
-let bins=512;
+let smoothing = 0.8;
+let bins = 512;
 let fft;
-let waveform=[];
-let r=100;
-let spectrum=[];
-let min=150;
-let distFromCenter=[];
+let waveform = [];
+let r = 100;
+let spectrum = [];
+let min = 150;
+let distFromCenter = [];
 
 
 
 var particles = [];
-let size=20;
-let num=10;
-let grid=[];
-let smoothedAvgFreq = 0; 
+let size = 20;
+let num = 10;
+let grid = [];
+let smoothedAvgFreq = 0;
+
+//Volume control by mic
+let mic;
+let vol = 0;
 
 
 
@@ -40,99 +44,117 @@ let smoothedAvgFreq = 0;
 function setup() {
 
     createCanvas(windowWidth, windowHeight, WEBGL);
-    
+
     colorMode(HSB, 255);
-    fft=new p5.FFT();
+    fft = new p5.FFT();
     angleMode(DEGREES);
-     for (let boxes=0; boxes<num; boxes++) {
-        grid[boxes]=[];
-        for (let boxes2=0; boxes2<num; boxes2++) {
-            grid[boxes][boxes2]=[];
-            for (let boxes3=0; boxes3<num; boxes3++){
-                grid[boxes][boxes2][boxes3]=floor(random(2));
+    for (let boxes = 0; boxes < num; boxes++) {
+        grid[boxes] = [];
+        for (let boxes2 = 0; boxes2 < num; boxes2++) {
+            grid[boxes][boxes2] = [];
+            for (let boxes3 = 0; boxes3 < num; boxes3++) {
+                grid[boxes][boxes2][boxes3] = floor(random(2));
 
-                let offset=size/2 -num/2*size
-                let x=boxes*size +offset;
-                let y=boxes2*size +offset;
-                let z=boxes3*size +offset;
-                let distance=dist(x,y,z,0,0,0);
+                let offset = size / 2 - num / 2 * size
+                let x = boxes * size + offset;
+                let y = boxes2 * size + offset;
+                let z = boxes3 * size + offset;
+                let distance = dist(x, y, z, 0, 0, 0);
 
-                distFromCenter.push({boxes,boxes2,boxes3,distance});
+                distFromCenter.push({ boxes, boxes2, boxes3, distance });
                 //console.log(distFromCenter);
 
             }
         }
-   }
-   distFromCenter.sort(compareDistances);
-   var buttonSpace=select('#buttonSpace');
+    }
+    distFromCenter.sort(compareDistances);
+    var buttonSpace = select('#buttonSpace');
 
-   //previous song button
-   prevSongButton=createButton('Previous Song');
-   prevSongButton.parent(buttonSpace);
-   prevSongButton.addClass('changeSong');
-   prevSongButton.mousePressed(()=>{
-    song.stop();
-    currentSongIndex=(currentSongIndex-1)% songs.length;
-    song=loadSound(songs[currentSongIndex], ()=>{
+    //previous song button
+    prevSongButton = createButton('Previous Song');
+    prevSongButton.parent(buttonSpace);
+    prevSongButton.addClass('changeSong');
+    prevSongButton.mousePressed(() => {
+        song.stop();
+        currentSongIndex = (currentSongIndex - 1) % songs.length;
+        song = loadSound(songs[currentSongIndex], () => {
+            song.play();
+            loop();
+            songSelector.selected(currentSongIndex);
+        });
+        console.log('Now playing:', songs[currentSongIndex]);
+    });
+    //play button
+    /* playButton = createButton('Play');
+    playButton.parent(buttonSpace);
+    playButton.addClass('playButton');
+    playButton.mousePressed(() => {
         song.play();
         loop();
         songSelector.selected(currentSongIndex);
-    });
-    console.log('Now playing:', songs[currentSongIndex]);
-   }); 
-   //play button
-   playButton = createButton('Play');
-   playButton.parent(buttonSpace);
-   playButton.addClass('playButton');
-   playButton.mousePressed(()=>{
-    song.play();
-    loop();
-    songSelector.selected(currentSongIndex);
-   });
+    }); */
 
-   //pause button
-   pauseButton = createButton('Pause');
-   pauseButton.parent(buttonSpace);
-   pauseButton.addClass('pauseButton')
-   pauseButton.mousePressed(() => {
-    song.pause();
-    noLoop();
-    songSelector.selected(currentSongIndex);
-    });
-
-   //change song button
-   changeSongButton = createButton('Next Song');
-   changeSongButton.parent(buttonSpace);
-   changeSongButton.addClass('changeSong');
-   changeSongButton.mousePressed(()=> {
-    song.stop();
-    currentSongIndex=(currentSongIndex+1) % songs.length;
-    song =loadSound(songs[currentSongIndex], ()=> {
-        song.play();
-        loop();
+    //toggle play/pause button. Icon source: https://emojidb.org/play-button-emojis
+    playButton=createButton('▶ Play');
+    playButton.parent(buttonSpace);
+    playButton.addClass('playButton');
+    playButton.mousePressed(()=>{
+        if(!song.isPlaying()) {
+            song.play();
+            loop();
+            playButton.html('◼ Pause');
+        } else {
+            song.pause();
+            noLoop();
+            playButton.html('▶ Play');
+        }
         songSelector.selected(currentSongIndex);
-        
+    })
+
+
+    //pause button
+    /* pauseButton = createButton('Pause');
+    pauseButton.parent(buttonSpace);
+    pauseButton.addClass('pauseButton')
+    pauseButton.mousePressed(() => {
+        song.pause();
+        noLoop();
+        songSelector.selected(currentSongIndex);
+    }); */
+
+    //change song button
+    changeSongButton = createButton('Next Song');
+    changeSongButton.parent(buttonSpace);
+    changeSongButton.addClass('changeSong');
+    changeSongButton.mousePressed(() => {
+        song.stop();
+        currentSongIndex = (currentSongIndex + 1) % songs.length;
+        song = loadSound(songs[currentSongIndex], () => {
+            song.play();
+            loop();
+            songSelector.selected(currentSongIndex);
+
+        });
+        console.log('Now playing:', songs[currentSongIndex]);
     });
-    console.log('Now playing:', songs[currentSongIndex]);
-   });
 
-   //Select songs button
-   let songSelector = createSelect();
-   songSelector.parent(buttonSpace);
-   songSelector.addClass('changeSong');
-   let songNames = [
-  "Renai Circulation-Kana Hanazawa", "Almond Chocolate-ILLIT", "Magnetic-ILLIT", "See Tinh-Hoang Thuy Linh", "Hyper Reality Show-Hatsune Miku",
-  "Tell Your World Remix-Hatsune Miku", "Kawaikute Gomen Remix-HoneyWorks", "SuMuZhe Remix-Zhang Xiaotan", "2 Phut Hon-Phao", "De Yang Gatal Gatal Sa-Bukan Pho DJ DESA Remix"
-];
+    //Select songs button
+    let songSelector = createSelect();
+    songSelector.parent(buttonSpace);
+    songSelector.addClass('changeSong');
+    let songNames = [
+        "Renai Circulation-Kana Hanazawa", "Almond Chocolate-ILLIT", "Magnetic-ILLIT", "See Tinh-Hoang Thuy Linh", "Hyper Reality Show-Hatsune Miku",
+        "Tell Your World Remix-Hatsune Miku", "Kawaikute Gomen Remix-HoneyWorks", "SuMuZhe Remix-Zhang Xiaotan", "2 Phut Hon-Phao", "De Yang Gatal Gatal Sa-Bukan Pho DJ DESA Remix"
+    ];
 
-// Populate the dropdown with song names
-   for (let i = 0; i < songs.length; i++) {
-    songSelector.option(songNames[i], i);
+    // Populate the dropdown with song names
+    for (let i = 0; i < songs.length; i++) {
+        songSelector.option(songNames[i], i);
     };
 
 
-// Handle song selection
-   songSelector.changed(() => {
+    // Handle song selection
+    songSelector.changed(() => {
         let selectedIndex = int(songSelector.value());
         if (selectedIndex !== currentSongIndex) {
             song.stop();
@@ -142,120 +164,120 @@ function setup() {
                 loop();
             });
             console.log('Now playing:', songs[currentSongIndex]);
-         }
-});
-   
+        }
+    });
 
-   
-   
+
+
+
 }
 
-function compareDistances(a,b) { //compare distance 
+function compareDistances(a, b) { //compare distance 
     return a.distance - b.distance;
 
 
 }
 function draw() {
-   //orbitControl(); //need to fix this
-   background(255,30);
-   fill(255,0,0);
-   
-   spectrum=fft.analyze();
-   let vol= fft.getEnergy(20,140);
-   if (vol>240) {
-      stroke(random(255),50,255);
-   } else if (vol>220 && vol<240) {
-        stroke(0,1,220);
+    //orbitControl(); //need to fix this
+    background(255, 30);
+    fill(255, 0, 0);
+
+    spectrum = fft.analyze();
+    let vol = fft.getEnergy(20, 140);
+    if (vol > 240) {
+        stroke(random(255), 50, 255);
+    } else if (vol > 220 && vol < 240) {
+        stroke(0, 1, 220);
         strokeWeight(0.3);
-   } else {
+    } else {
         noStroke();
-   }
-   
-   let totalCubes=num*num*num;
-   /* for (let i=0; i<totalCubes; i++) {
-    let pos=distFromCenter[i];
-    let color=map(spectrum[i],0, 255, min,255);
-    grid[pos.boxes][pos.boxes2][pos.boxes3]=color;
-   } *///Purple-ish-Original tutorial
-   for (let i = 0; i < totalCubes; i++) {
-    let pos = distFromCenter[i];
-    // Map the spectrum value to a hue (0-255)
-    let hue = map(spectrum[i], 0, 255, 0, 255); 
-    // For audio-reactive rainbow: 
-    grid[pos.boxes][pos.boxes2][pos.boxes3] = hue;
-}
-
-
-
-   noFill();
-   let offset=size/2 -num/2*size
-   translate(offset, offset, offset);
-  /*  for (let boxes=0; boxes<num; boxes++) {
-        for (let boxes2=0; boxes2<num; boxes2++) {
-            for (let boxes3=0; boxes3<num; boxes3++){
-                if (grid[boxes][boxes2][boxes3]>min) {
-                    fill(grid[boxes][boxes2][boxes3],0,200);
-                } else {
-                    noFill();
-                }
-                
-                push();
-                translate(boxes*size, boxes2*size, boxes3*size);
-                box(size- size/4);
-                pop();
-            }
-            
-        }
-   } */
-   let avgFreq = 0;
-   for (let i = 0; i < spectrum.length; i++) {
-    avgFreq += spectrum[i];
     }
-   avgFreq /= spectrum.length;
 
-   // Smooth the average frequency for less jitter
-   smoothedAvgFreq = lerp(smoothedAvgFreq, avgFreq, 0.1);
-   // Map average frequency to rotation angles
-   let rotX = map(smoothedAvgFreq*10, 0, 255, 0, PI)+frameCount * 0.01;;
-   let rotY = map(smoothedAvgFreq*20, 0, 255, 0, TWO_PI) + frameCount * 0.013;
-   let rotZ = map(smoothedAvgFreq*20, 0, 255, 0, PI) +  frameCount * 0.008 ;
+    let totalCubes = num * num * num;
+    /* for (let i=0; i<totalCubes; i++) {
+     let pos=distFromCenter[i];
+     let color=map(spectrum[i],0, 255, min,255);
+     grid[pos.boxes][pos.boxes2][pos.boxes3]=color;
+    } *///Purple-ish-Original tutorial
+    for (let i = 0; i < totalCubes; i++) {
+        let pos = distFromCenter[i];
+        // Map the spectrum value to a hue (0-255)
+        let hue = map(spectrum[i], 0, 255, 0, 255);
+        // For audio-reactive rainbow: 
+        grid[pos.boxes][pos.boxes2][pos.boxes3] = hue;
+    }
 
-   push();
-   rotateX(rotX);
-   rotateY(rotY);
-   rotateZ(rotZ);
-   for (let boxes=0; boxes<num; boxes++) {
-        for (let boxes2=0; boxes2<num; boxes2++) {
-            for (let boxes3=0; boxes3<num; boxes3++){
-                if (grid[boxes][boxes2][boxes3]>min) {
-                    fill(grid[boxes][boxes2][boxes3],60,255);
+
+
+    noFill();
+    let offset = size / 2 - num / 2 * size
+    translate(offset, offset, offset);
+    /*  for (let boxes=0; boxes<num; boxes++) {
+          for (let boxes2=0; boxes2<num; boxes2++) {
+              for (let boxes3=0; boxes3<num; boxes3++){
+                  if (grid[boxes][boxes2][boxes3]>min) {
+                      fill(grid[boxes][boxes2][boxes3],0,200);
+                  } else {
+                      noFill();
+                  }
+                  
+                  push();
+                  translate(boxes*size, boxes2*size, boxes3*size);
+                  box(size- size/4);
+                  pop();
+              }
+              
+          }
+     } */
+    let avgFreq = 0;
+    for (let i = 0; i < spectrum.length; i++) {
+        avgFreq += spectrum[i];
+    }
+    avgFreq /= spectrum.length;
+
+    // Smooth the average frequency for less jitter
+    smoothedAvgFreq = lerp(smoothedAvgFreq, avgFreq, 0.1);
+    // Map average frequency to rotation angles
+    let rotX = map(smoothedAvgFreq * 10, 0, 255, 0, PI) + frameCount * 0.01;;
+    let rotY = map(smoothedAvgFreq * 20, 0, 255, 0, TWO_PI) + frameCount * 0.013;
+    let rotZ = map(smoothedAvgFreq * 20, 0, 255, 0, PI) + frameCount * 0.008;
+
+    push();
+    rotateX(rotX);
+    rotateY(rotY);
+    rotateZ(rotZ);
+    for (let boxes = 0; boxes < num; boxes++) {
+        for (let boxes2 = 0; boxes2 < num; boxes2++) {
+            for (let boxes3 = 0; boxes3 < num; boxes3++) {
+                if (grid[boxes][boxes2][boxes3] > min) {
+                    fill(grid[boxes][boxes2][boxes3], 60, 255);
                 } else {
                     noFill();
                 }
-                
+
                 push();
-                translate(boxes*size, boxes2*size, boxes3*size);
-                box(size- size/4);
+                translate(boxes * size, boxes2 * size, boxes3 * size);
+                box(size - size / 4);
                 pop();
             }
-            
+
         }
-   }
+    }
     pop();
 
-   
-   
 
-   fft.analyze();
-   amp=fft.getEnergy(20, 200);
-   if (amp > 200) {
-    particles.push(new Particle());
+
+
+    fft.analyze();
+    amp = fft.getEnergy(20, 200);
+    if (amp > 200) {
+        particles.push(new Particle());
     }
-   
 
 
 
-  
+
+
 
     for (let i = particles.length - 1; i >= 0; i--) {
         particles[i].update(amp > 210);
@@ -264,22 +286,22 @@ function draw() {
             particles.splice(i, 1);
         }
     }
-    
+
 
 }
 
 
 
-class Particle{
-    constructor(){
+class Particle {
+    constructor() {
         let r = 200;
         this.pos = p5.Vector.random2D().mult(r);
-        this.vel = createVector(0,0);
-        this.acc=this.pos.copy().normalize().mult(random(0.1,0.5));
+        this.vel = createVector(0, 0);
+        this.acc = this.pos.copy().normalize().mult(random(0.1, 0.5));
 
-        this.w=random(8, 16);
-        this.hue=random(180, 255);//pastel
-        this.lifetime=255; //fading out
+        this.w = random(8, 16);
+        this.hue = random(180, 255);//pastel
+        this.lifetime = 255; //fading out
 
     }
     update(cond) {
@@ -291,19 +313,19 @@ class Particle{
             this.pos.add(this.vel);
             this.pos.add(this.vel);
         }
-        this.lifetime -=3;
+        this.lifetime -= 3;
 
     }
 
     show() {
         noStroke();
-        fill(this.hue,60,255,this.lifetime);
+        fill(this.hue, 60, 255, this.lifetime);
         ellipse(this.pos.x, this.pos.y, this.w);
     }
     isDead() {
-        return this.lifetime<0;
+        return this.lifetime < 0;
     }
-    
+
 }
 
 
